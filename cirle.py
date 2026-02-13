f378b58ad61b275d8fdb1145357d8b4abc889b26
Ariyan Mondal <ariyanmondalofficial@gmail.com>
2026-02-13 10:44:33 +0530

    animated circle

---
+++ cirle.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+
+circle = plt.Circle((9, 1), 1)
+
+ax.add_patch(circle)
+
+def init():
+    circle.center = (0, 5)
+    return circle,
+
+def animate(frame):
+    x = frame * 0.1
+    y = 5
+
+    circle.center = ( x,y)
+    return circle,
+
+ani = animation.FuncAnimation(
+    fig,
+    animate,
+    frames=100,
+    init_func=init,
+    interval=30,
+    blit=True
+)
+plt.show()
