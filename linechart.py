68086af5a64c8adabfe04692cf479f818c2eadaf
Ariyan Mondal <ariyanmondalofficial@gmail.com>
2026-02-20 09:56:34 +0530

    line chart

---
+++ linechart.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 15, 7, 20, 18]
+
+plt.plot(x, y)
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Simple Line Chart")
+plt.show()
