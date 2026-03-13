24f363a0245e123b38c9e9ead0c67145afeb73f7
Ariyan Mondal <ariyanmondalofficial@gmail.com>
2026-03-13 09:55:34 +0530

    variables

---
+++ dyvariable.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import random
+
+x = []
+y = []
+
+fig, ax = plt.subplots()
+
+def update(frame):
+    x.append(frame)
+    y.append(random.randint(10, 30))
+
+    ax.clear()
+    ax.plot(x, y, marker='o')
+
+    ax.set_xlabel("X values (Independent Variable)")
+    ax.set_ylabel("Y values (Dependent Variable)")
+    ax.set_title("Live Animated X-Y Plot")
+    ax.grid(True)
+
+ani = FuncAnimation(fig, update, interval=1000)
+
+plt.show()
